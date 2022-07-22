56c1df070fb45b3698c1fce1e79af6930310b5b8
Bryan Thomas <49354825+bryanjtc@users.noreply.github.com>
2022-07-22 00:28:57 -0500

    Agregue las barras de vida funcionando

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -8,10 +8,24 @@ public class EnemyController : MonoBehaviour
 
     private Animator anim;
 
+    public int maxHealth = 100;
+	public int currentHealth;
+    public HealthBar healthBar;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+		healthBar.SetMaxHealth(maxHealth);
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         anim.SetTrigger("hit");
     }
 
-
+    public void TakeDamage(int damage)
+	{
+		currentHealth -= damage;
+		healthBar.SetHealth(currentHealth);
+	}
 }
