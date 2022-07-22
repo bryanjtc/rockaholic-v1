b734c868226a91990dd684b5b6cd5fd852d4f9d5
tonioqiu <tonioqiu.dev@gmail.com>
2022-07-22 02:07:51 -0500

    Agregado Escena de Victory y Game Over

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -8,6 +8,12 @@ public class MainMenu : MonoBehaviour
 
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(1);
     }
+
+    public void ReMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
 }
