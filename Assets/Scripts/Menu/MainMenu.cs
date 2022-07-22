f8529343eb3a2317791caf1a69e8209e5a85c7f0
ExBeNN <sergiojoserc@gmail.com>
2022-07-22 01:28:45 -0500

    Sonidos menu wow

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -5,9 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public float delayTime = 2f;
 
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        Invoke("DelayedAction", delayTime);
+    }
+
+    void DelayedAction(){
+       SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
